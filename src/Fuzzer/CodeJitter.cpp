d8fc19e2a7f14575feb1685f8b79460e820709f5
Max Mathys <max.mathys@gmail.com>
2021-12-14 16:39:44 +0100

    non-simd, but fastest version

--- src/Fuzzer/CodeJitter.cpp
+++ src/Fuzzer/CodeJitter.cpp
@@ -175,7 +175,7 @@ void CodeJitter::jit_strict(int num_acts_per_trefi,
       a_vec.push_back((uint64_t) inc_a.to_virt());
     }
 
-    //std::sort(a_vec.begin(), a_vec.end());
+    std::sort(a_vec.begin(), a_vec.end());
 
     for(int j = 0; j < 4; j++) {
       uint64_t addr = a_vec[j];
@@ -194,11 +194,6 @@ void CodeJitter::jit_strict(int num_acts_per_trefi,
       }
     }
 
-    for (int j = 0; j < 4; j++) {
-      //a.mov(asmjit::x86::r8, a_vec[j] - a_vec[0]); // write offset into r8
-      //a.vpinsrq(asmjit::x86::xmm1, asmjit::x86::xmm1, asmjit::x86::r8, asmjit::Imm(j));
-    }
-
     // hammer
     a.mov(asmjit::x86::rax, a_vec[0]);
     a.mov(asmjit::x86::r8, a_vec[1]);
@@ -209,10 +204,9 @@ void CodeJitter::jit_strict(int num_acts_per_trefi,
     a.mov(asmjit::x86::r12, asmjit::x86::ptr(asmjit::x86::r9));
     a.mov(asmjit::x86::r13, asmjit::x86::ptr(asmjit::x86::r10));
     
-    //a.vpcmpeqw(asmjit::x86::xmm3, asmjit::x86::xmm3, asmjit::x86::xmm3);     // xmm3: mask to all 1s
-    //a.vxorpd(asmjit::x86::xmm1, asmjit::x86::xmm1, asmjit::x86::xmm1);
-    //asmjit::x86::Mem vx_ptr = asmjit::x86::ptr(asmjit::x86::rax, asmjit::x86::xmm1, 1);
-    //a.vgatherqpd(asmjit::x86::xmm0, vx_ptr, asmjit::x86::xmm3);
+    a.vpcmpeqw(asmjit::x86::ymm3, asmjit::x86::ymm3, asmjit::x86::ymm3);     // xmm3: mask to all 1s
+    asmjit::x86::Mem vx_ptr = asmjit::x86::ptr(asmjit::x86::rax, asmjit::x86::ymm1, 1);
+    a.vgatherqpd(asmjit::x86::ymm0, vx_ptr, asmjit::x86::ymm3);
 
     accessed_before[cur_addr] = true;
     a.dec(asmjit::x86::rsi);
